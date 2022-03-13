1ff739076cc6bb2fd5f9bd3e44426c38a95fad1e
Lexi Dorrity <lexidorrity@Lexis-MBP.socal.rr.com>
2022-03-13 11:22:13 -0700

    added function for coloring boxes and save button

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,28 @@
-//grabbing the save button as an element
-var saveBtn = $('#saveBtn');
+
+
 // var plannerItem = $('#plannerItem')
 
 //When you click the save button plannerItem needs to be saved in local storage, 
 //when you open the page past hours will be grey, present hour will be red and future hours will be green
 var plannerTime = $('#plannerItem')
 var currentTime = moment().format("hh:mm:ss");
-var storeSave = $('#saveBtn');
 var currentHour = moment().format("HH")
-console.log(currentHour)
-console.log($(".time-block").data())
+var itemImput = $('.itemImput').val()
+//When you open the page the current day is displayed at the top of the page
+var time = moment().format("MMM Do, YYYY");
+$('#currentDay').text(time)
+// save button allows user to save a task to local storage
+$('.saveBtn').each(function(i, saveBtn){
+$(saveBtn).click(function(event) {
+    event.preventDefault();
+    localStorage.setItem(i,$(`#plannerItem${i}`).val())
+    // localStorage.setItem(timeBtn.length);
+    console.log(localStorage);
+    console.log($(`#plannerItem${i}`).val())
+    console.log(i, event.target)
+    //save users input from the text area into local storage 
+})
 
-// made an array for all of the time block elements and for loop to cycyle through them
-var timeBlockElementArray = $(".time-block")
-for (var i = 0; i < timeBlockElementArray.length; i++) {
-    console.log($(timeBlockElementArray[i]).data())
-}
 if (currentHour === $('.time-block')) {
     $('.time-block').attr('data-time')
 }
@@ -38,12 +45,6 @@ $('.time-block').each(function(i, div) {
 
 
 
-var text
-localStorage.setItem
+// userFormEl.addEventListener('saveBtn', formSubmitHandler);
 
-//When you open the page the current day is displayed at the top of the page
-var time = moment().format("MMM Do, YYYY");
-$('#currentDay').text(time)
-
-// document.setAttribute($(timeBlockElementArray[i].data()))
-//document.getAttribute('time-block);
+})
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # planner
+
+var saveBtnElementArray = $('.saveBtnArr')
+for (var i = 0; i < saveBtnElementArray.length; i++) {
+    console.log($(saveBtnElementArray[i]).data())
+
+    $('.saveBtnArr').each(function(i, button) {
+        
+        })
+
+    })
