156680d027caa4ea917ea509bfbea9dc108f0783
Lexi Dorrity <lexidorrity@Lexis-MacBook-Pro.local>
2022-03-13 17:47:59 -0700

    added render last registered function and get item function

--- Assets/script.js
+++ Assets/script.js
@@ -8,26 +8,11 @@ var plannerTime = $('#plannerItem')
 var currentTime = moment().format("hh:mm:ss");
 var currentHour = moment().format("HH")
 var itemImput = $('.itemImput').val()
-//When you open the page the current day is displayed at the top of the page
-var time = moment().format("MMM Do, YYYY");
-$('#currentDay').text(time)
-// save button allows user to save a task to local storage
-$('.saveBtn').each(function(i, saveBtn){
-$(saveBtn).click(function(event) {
-    event.preventDefault();
-    localStorage.setItem(i,$(`#plannerItem${i}`).val())
-    // localStorage.setItem(timeBtn.length);
-    console.log(localStorage);
-    console.log($(`#plannerItem${i}`).val())
-    console.log(i, event.target)
-    //save users input from the text area into local storage 
-})
 
 if (currentHour === $('.time-block')) {
     $('.time-block').attr('data-time')
 }
-// console.log($('.time-block').attr('data-time')
-// )
+
 $('.time-block').each(function(i, div) {
     console.log(currentHour)
     console.log($(div).attr('data-time'))
@@ -39,8 +24,35 @@ $('.time-block').each(function(i, div) {
     }
     else if (currentHour < $(div).attr('data-time')) {
         $(div).attr('class', 'future')
+    
     }
+
+renderLastRegistered();
+
+function renderLastRegistered() {
+    localStorage.getItem(i,$(`#plannerItem${i}`).val());
+}
+
+//When you open the page the current day is displayed at the top of the page
+var time = moment().format("MMM Do, YYYY");
+$('#currentDay').text(time)
+// save button allows user to save a task to local storage
+
+
+$('.saveBtn').each(function(i, saveBtn){
+$(saveBtn).click(function(event) {
+    event.preventDefault();
+    localStorage.setItem(i,$(`#plannerItem${i}`).val())
+    // localStorage.setItem(timeBtn.length);
+    console.log(localStorage);
+    console.log($(`#plannerItem${i}`).val())
+    console.log(i, event.target)
+    //save users input from the text area into local storage 
 })
+
+
+})
+renderLastRegistered();
 
 
 
